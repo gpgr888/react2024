23a3bd1ca4762b84e052067afbb31fc9bcfbe6b1
gpgr888 <poudelgaurav@gmail.com>
2024-02-02 14:04:36 +0545

    imports and exports concepts

--- 03-jsrefresher/assets/scripts/app.js
+++ 03-jsrefresher/assets/scripts/app.js
@@ -1,1 +1,30 @@
 
+//explore import and export
+// if we need something from util.js we need to import it, multiple variables can be inside {} braces to import and typically relative path
+// in react though extension is not needed
+//for multiple named imports we can use either below method inside braces or 
+
+  // import { apikey, multile } from "./util.js";
+
+
+//you can also use alias if you want to change the name of variable with as keyword like below
+
+// import {apikey as key, multiple} from "./util.js";
+
+//or below method where all the vairable of util.js are groupded inside util variable and we use . operator to access those variables
+import * as util from "./util.js";
+
+//for default import we dont need braces
+
+import apikey2 from "./util.js";
+
+
+//for multipel imports which are named exports with vairable,
+
+
+console.log(util.apikey);
+console.log(apikey2);
+console.log(util.multile);
+
+// if we run this code then we error "Cannot use import statement outside a module" this is because we have not put type="module" in our html file where
+//we have called this file in script tag so if we have to use import and export we have to use type="module" in script tag 
---
+++ 03-jsrefresher/assets/scripts/util.js
@@ -0,0 +1,8 @@
+//dummy export if this need to be made available to app.js it needs to be exported using export keyword, below is one way of exporting we have many
+export let apikey = 'Testkey';
+
+//second using default, and there should be only one default export in one file, and with default we dont need variable we can directly export value
+
+export default "TestKeyDefaultExport";
+
+export let multile = "abc";
