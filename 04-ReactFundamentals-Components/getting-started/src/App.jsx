73ed9d6f2e09a228de0a3e55c030748cfc1227f2
gpgr888 <poudelgaurav@gmail.com>
2024-02-06 22:36:39 +0545

    dynamic data and loading dynamic images

--- 04-ReactFundamentals-Components/getting-started/src/App.jsx
+++ 04-ReactFundamentals-Components/getting-started/src/App.jsx
@@ -1,13 +1,21 @@
+import heroImage from './assets/react-core-concepts.png';
+
+const dynamicDescriptions = ['Fundamentals','Crucial','Core','Valuable'];
+
+function getRamdomIndex(max){
+  return Math.floor(Math.random() * (max + 1));
+}
 function Header(){
+  const description = dynamicDescriptions[getRamdomIndex(3)];
   return (
        <header>
            
-           <img src="src/assets/react-core-concepts.png" alt="hero logo" />
+           <img src={heroImage} alt="hero logo" />
            
            <h1>React Fundamentals</h1>
            
            <p>
-             Fundamental React concepts you will need for almost any app you are going to build!!
+             {description} React concepts you will need for almost any app you are going to build!!
            </p>
         
         </header>
--- 04-ReactFundamentals-Components/getting-started/src/index.jsx
+++ 04-ReactFundamentals-Components/getting-started/src/index.jsx
@@ -1,10 +1,24 @@
+// this is the main file , this is where our react app boots up
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// to be precise these ReactDOM library is responsible for rendering , our jsx code is passed to this library, 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// below we are getting a main div id where we are trying to mount our app code and then using ReacDOM library and and its render
+// funcion we render our <App /> component
+
+const mainPoint = document.getElementById("root");
+ReactDOM.createRoot(mainPoint).render(<App />);
+// ReactDOM.createRoot(document.getElementById('root')).render( 
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+
+// oour App component may contain many child component and child component may contain many nested components which makes a component tree 
+// and hirarchy but in frontend we dont them as nested component we see a regular html tags and all as react converts our jxs to regular html
+
+// NOTE : ALL THE BUILD IN COMPONENTS LIKE DIV,HEADER,MAIN,SECTION ETC ARE IN LOWER CASE WHERE AS OUR CUSTOM COMPONENTS MUST START WITH UPPER CASE
