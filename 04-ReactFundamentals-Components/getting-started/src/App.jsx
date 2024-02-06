f0ed6fddf74ea8707b50ce1212aeda548e3fec5e
gpgr888 <poudelgaurav@gmail.com>
2024-02-06 12:06:17 +0545

    react fundamentals

---
+++ 04-ReactFundamentals-Components/getting-started/src/App.jsx
@@ -0,0 +1,31 @@
+function App(){
+
+  return (
+
+      <div>
+        <header>
+           
+           <img src="src/assets/react-core-concepts.png" alt="hero logo" />
+           
+           <h1>React Fundamentals</h1>
+           
+           <p>
+             Fundamental React concepts you will need for almost any app you are going to build!!
+           </p>
+        
+        </header>
+
+        <main>
+          
+        
+          <h2>Time to get started!</h2>
+
+
+        </main>
+
+      </div>
+
+    );
+}
+
+export default App;
