81677f1346f02022ed98fe868eb389da49c145be
gpgr888 <poudelgaurav@gmail.com>
2024-02-08 12:24:10 +0545

    dive deep

--- 05-ReactFundamentals-Components-Deep/getting-started/src/components/Header/Header.jsx
+++ 05-ReactFundamentals-Components-Deep/getting-started/src/components/Header/Header.jsx
@@ -1,27 +1,23 @@
-// It is to note that though we seperate our style to component specific it is not automatically limited to component scope
-// meaning these styles are valid for any other elements which has class or tag outside this component
-// later we will be learning to solve this limitation and we will implement component scope styles
+import reactImg from '../../assets/react-core-concepts.png';
 import './Header.css';
-import heroImage from '../../assets/react-core-concepts.png'
-const dynamicDescriptions = ['Fundamentals','Crucial','Core','Valuable'];
 
-function getRamdomIndex(max){
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-export default function Header(){
-  const description = dynamicDescriptions[getRamdomIndex(3)];
+export default function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
-       <header>
-           
-           <img src={heroImage} alt="hero logo" />
-           
-           <h1>React Fundamentals</h1>
-           
-           <p>
-             {description} React concepts you will need for almost any app you are going to build!!
-           </p>
-        
-        </header>
-    )
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
 }
