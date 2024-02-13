6c5b07123f562d0c903f5f45dc549f8bd929e545
gpgr888 <poudelgaurav@gmail.com>
2024-02-13 09:10:12 +0545

    splitting components by feature and state

--- 05-ReactFundamentals-Components-Deep/getting-started/src/App.jsx
+++ 05-ReactFundamentals-Components-Deep/getting-started/src/App.jsx
@@ -1,36 +1,22 @@
 import { useState , Fragment} from 'react';
 
-import { CORE_CONCEPTS } from './data.js';
+
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import { EXAMPLES } from './data.js';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
+   // Spliting Components
 
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
-  }
-
-  console.log('APP COMPONENT EXECUTING');
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
+    // as you may have noticed that in front end if we click any tab component our header component description changes 
+  // this is because as we click our tab component our this manged state changes and since it is in main App function whole 
+  // dom gets re-rendered as We have called <Header>/> component here it also re-renders so we kind understand that this state we have
+  // implemented in wrong place, so it is important to split components by feature and state
+  
+  // now as we have splited our components to individual components now we dont see header change when tab button is clicked
   return (
     // here if we omit <div> it will give error cause if we omit div react will think think there are multiple layers in return 
     // as we know that we cannot return more than one value with return statement, because at the end 
@@ -41,47 +27,13 @@ function App() {
     //   ) so here we get two elements, so we need one wrapping element, which technically return one value
     // this gives us an extra div in our dom , to void this unnecessary div react gives us a fragment component 
     // which we need to import
+
+
     <Fragment>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === 'components'}
-              onSelect={() => handleSelect('components')}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'jsx'}
-              onSelect={() => handleSelect('jsx')}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'props'}
-              onSelect={() => handleSelect('props')}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'state'}
-              onSelect={() => handleSelect('state')}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </Fragment>
   );
